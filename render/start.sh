eb9d36d19f6bcd489fe312a054a7d5d1b6072ddf
nyaabaaugustine <nyaabaaugustine@gmail.com>
2025-12-29 02:44:27 +0000

    Embed startup command in Dockerfile and remove start.sh

--- render/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-# Ensure log directory exists and has correct permissions
-mkdir -p /var/log/icecast2
-chown -R icecast2:icecast /var/log/icecast2
-# Start Icecast in the foreground
-echo "Starting Icecast server..."
-icecast2 -c /app/icecast.xml
